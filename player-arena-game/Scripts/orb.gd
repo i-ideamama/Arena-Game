c64a44e1ecb596c9da1740ad3333c5b6b314f135
Advaith Neelacantan <advaith.n@gmail.com>
2025-02-07 02:06:41 +0530

    feat: updated orb assets

--- player-arena-game/Scripts/orb.gd
+++ player-arena-game/Scripts/orb.gd
@@ -3,7 +3,8 @@ extends Node2D
 var powerup
 
 func _ready() -> void:
-	var random = RandomNumberGenerator.new()
-	random.randomize()
-	var orb_sprite_number = randi() % 18
-	$Sprite2D.texture = load("res://Assets/Orbs/Orb_"+str(orb_sprite_number)+".png")
+	$AnimationPlayer.play("spin")
+	#var random = RandomNumberGenerator.new()
+	#random.randomize()
+	#var orb_sprite_number = randi() % 18
+	#$Sprite2D.texture = load("res://Assets/Orbs/Orb_"+str(orb_sprite_number)+".png")
